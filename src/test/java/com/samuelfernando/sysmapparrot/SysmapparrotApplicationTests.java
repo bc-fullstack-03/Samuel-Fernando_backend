06c4dc82ed9f4de8d7bf977eb16ef089503cfbd7
SamuelFST <guti.samuel2002@gmail.com>
2023-05-01 13:08:33 -0300

    initial commit

---
+++ src/test/java/com/samuelfernando/sysmapparrot/SysmapparrotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelfernando.sysmapparrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysmapparrotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
