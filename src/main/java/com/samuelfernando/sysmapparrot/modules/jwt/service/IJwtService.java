27d6d29b99993ec237a1605260a6e21e12675886
SamuelFST <guti.samuel2002@gmail.com>
2023-05-03 21:58:35 -0300

    function to validate the informed jwt token

--- src/main/java/com/samuelfernando/sysmapparrot/modules/jwt/service/IJwtService.java
+++ src/main/java/com/samuelfernando/sysmapparrot/modules/jwt/service/IJwtService.java
@@ -4,4 +4,5 @@ import java.util.UUID;
 
 public interface IJwtService {
 	String generateToken(UUID userId);
+	String validateToken(String token);
 }
--- src/main/java/com/samuelfernando/sysmapparrot/modules/jwt/service/JwtService.java
+++ src/main/java/com/samuelfernando/sysmapparrot/modules/jwt/service/JwtService.java
@@ -1,5 +1,7 @@
 package com.samuelfernando.sysmapparrot.modules.jwt.service;
 
+import static org.springframework.util.ObjectUtils.isEmpty;
+
 import java.security.Key;
 import java.util.Date;
 import java.util.UUID;
@@ -7,6 +9,9 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.samuelfernando.sysmapparrot.config.exception.AuthenticationException;
+
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -28,7 +33,42 @@ public class JwtService implements IJwtService {
 				.compact();
 	}
 	
+	public String validateToken(String token) {
+		String accessToken = extractToken(token);
+
+		try {
+			Claims claims = Jwts
+					.parserBuilder()
+					.setSigningKey(generateKey())
+					.build()
+					.parseClaimsJws(accessToken)
+					.getBody();
+				
+			String subject = claims.getSubject();
+			
+			if (isEmpty(subject) || claims.getExpiration().before(new Date())) {
+				throw new AuthenticationException("Invalid token");
+			}
+					
+			return subject;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			throw new AuthenticationException("Error while trying to proccess the informed access token");
+		}
+	}
+	
 	private Key generateKey() {
 		return Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY));
+	}
+	
+	private String extractToken(String token) {
+		if (isEmpty(token)) {
+			throw new AuthenticationException("Access token was not informed");
+		}
+		if (token.contains(" ")) {
+			return token.split(" ")[1];
+		}
+		
+		return token;
 	}
 }
