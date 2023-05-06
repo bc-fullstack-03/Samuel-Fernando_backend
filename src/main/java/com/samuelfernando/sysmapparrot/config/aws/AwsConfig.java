3b8d74460439915a9331aae35ebb53429db558d7
SamuelFST <guti.samuel2002@gmail.com>
2023-05-06 12:40:10 -0300

    aws config and service created

---
+++ src/main/java/com/samuelfernando/sysmapparrot/config/aws/AwsConfig.java
@@ -0,0 +1,31 @@
+package com.samuelfernando.sysmapparrot.config.aws;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class AwsConfig {
+	@Value("${app-config.secrets.aws-access-key}")
+	private String accessKey;
+	@Value("${app-config.secrets.aws-secret-key}")
+	private String secretKey;
+	@Value("${app-config.secrets.aws-s3-url}")
+	private String s3Url;
+	
+	@Bean
+	public AmazonS3 amazonS3() {
+		return AmazonS3ClientBuilder
+				.standard()
+				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+				.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(s3Url, Regions.US_WEST_2.getName()))
+				.build();
+	}
+}
---
+++ src/main/java/com/samuelfernando/sysmapparrot/modules/aws/service/AwsService.java
@@ -0,0 +1,56 @@
+package com.samuelfernando.sysmapparrot.modules.aws.service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Service
+public class AwsService {
+
+	@Autowired
+	private AmazonS3 amazonS3;
+
+	@Value("${app-config.secrets.aws-s3-url}")
+	private String s3Url;
+	@Value("${app-config.secrets.aws-bucket-name}")
+	private String bucketName;
+
+	public String upload(MultipartFile multipartFile, String filename) throws Exception {
+		String fileUri = "";
+
+		try {
+			File convertedFile = convertMultipartToFile(multipartFile);
+			amazonS3.putObject(new PutObjectRequest(bucketName, filename, convertedFile)
+					.withCannedAcl(CannedAccessControlList.PublicRead));
+			
+			fileUri = s3Url + "/" + bucketName + "/" + filename;
+			
+			convertedFile.delete();
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+
+		return fileUri;
+	}
+
+	private File convertMultipartToFile(MultipartFile file) throws IOException {
+		File convertedFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
+
+		FileOutputStream fileOutputStream = new FileOutputStream(convertedFile);
+
+		fileOutputStream.write(file.getBytes());
+		fileOutputStream.close();
+
+		return convertedFile;
+	}
+}
