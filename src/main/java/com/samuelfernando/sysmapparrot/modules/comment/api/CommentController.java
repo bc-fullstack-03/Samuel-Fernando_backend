665af258a53d32f5993514af569d7df401184e6c
SamuelFST <guti.samuel2002@gmail.com>
2023-05-07 20:54:24 -0300

    comment controller created and all comment resources moved from post controller

---
+++ src/main/java/com/samuelfernando/sysmapparrot/modules/comment/api/CommentController.java
@@ -0,0 +1,63 @@
+package com.samuelfernando.sysmapparrot.modules.comment.api;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.samuelfernando.sysmapparrot.modules.comment.dto.CommentRequest;
+import com.samuelfernando.sysmapparrot.modules.comment.dto.CommentResponse;
+import com.samuelfernando.sysmapparrot.modules.comment.dto.UserCommentResponse;
+import com.samuelfernando.sysmapparrot.modules.comment.service.ICommentService;
+
+@RestController
+@RequestMapping("/api/v1/post")
+public class CommentController {
+	@Autowired
+	private ICommentService commentService;
+	
+	@GetMapping("/{id}/comments")
+	public CommentResponse getComments(@PathVariable UUID id) {
+		return commentService.getComments(id);
+	}
+	
+	@GetMapping("/{id}/comments/{commentId}")
+	public UserCommentResponse getComment(@PathVariable UUID id, @PathVariable UUID commentId) {
+		return commentService.getComment(id, commentId);
+	}
+	
+	@PostMapping("/{id}/comments/{commentId}/like")
+	public void likePostComment(@PathVariable UUID id, @PathVariable UUID commentId) {
+		commentService.likePostComment(id, commentId);
+	}
+	
+	@PostMapping("/{id}/comments/{commentId}/unlike")
+	public void unlikePostComment(@PathVariable UUID id, @PathVariable UUID commentId) {
+		commentService.unlikePostComment(id, commentId);
+	}
+	
+	@PostMapping("/{id}/comments")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void insertPostComment(@PathVariable UUID id, @RequestBody CommentRequest commentRequest) {
+		commentService.insertPostComment(id, commentRequest);
+	}
+	
+	@PutMapping("/{id}/comments/{userCommentId}")
+	public void updateUserComment(@PathVariable UUID id, @PathVariable UUID userCommentId, @RequestBody CommentRequest commentRequest) {
+		commentService.updateUserComment(id, userCommentId, commentRequest);
+	}
+	
+	@DeleteMapping("/{id}/comments/{userCommentId}")
+	public void deletePost(@PathVariable UUID id, @PathVariable UUID userCommentId) {
+		commentService.deleteUserComment(id, userCommentId);
+	}
+}
--- src/main/java/com/samuelfernando/sysmapparrot/modules/post/api/PostController.java
+++ src/main/java/com/samuelfernando/sysmapparrot/modules/post/api/PostController.java
@@ -10,17 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.samuelfernando.sysmapparrot.modules.comment.dto.CommentRequest;
-import com.samuelfernando.sysmapparrot.modules.comment.dto.CommentResponse;
-import com.samuelfernando.sysmapparrot.modules.comment.dto.UserCommentResponse;
-import com.samuelfernando.sysmapparrot.modules.comment.service.ICommentService;
 import com.samuelfernando.sysmapparrot.modules.post.dto.CreatePostRequest;
 import com.samuelfernando.sysmapparrot.modules.post.dto.PostResponse;
 import com.samuelfernando.sysmapparrot.modules.post.dto.UpdatePostRequest;
@@ -31,9 +26,7 @@ import com.samuelfernando.sysmapparrot.modules.post.service.IPostService;
 public class PostController {
 	@Autowired
 	private IPostService postService;
-	@Autowired
-	private ICommentService commentService;
-
+	
 	@GetMapping("")
 	public List<PostResponse> getMyPosts() {
 		return postService.getMyPosts();
@@ -43,17 +36,6 @@ public class PostController {
 	public PostResponse getPostById(@PathVariable UUID id) {
 		return postService.getPostById(id);
 	}
-
-	@GetMapping("/{id}/comments")
-	public CommentResponse getComments(@PathVariable UUID id) {
-		return commentService.getComments(id);
-	}
-	
-	@GetMapping("/{id}/comments/{commentId}")
-	public UserCommentResponse getComment(@PathVariable UUID id, @PathVariable UUID commentId) {
-		return commentService.getComment(id, commentId);
-	}
-
 
 	@PostMapping("")
 	@ResponseStatus(HttpStatus.CREATED)
@@ -72,30 +54,14 @@ public class PostController {
 		postService.unlikePost(id);
 	}
 	
-	@PostMapping("/{id}/comments")
-	@ResponseStatus(HttpStatus.CREATED)
-	public void insertPostComment(@PathVariable UUID id, @RequestBody CommentRequest commentRequest) {
-		commentService.insertPostComment(id, commentRequest);
-	}
-
 	@PutMapping("/{id}")
 	public void updatePost(@PathVariable UUID id, @RequestPart(required = false, name = "photo") MultipartFile photo,
 			@RequestPart("post") UpdatePostRequest updatePostRequest) {
 		postService.updatePost(id, updatePostRequest, photo);
 	}
 	
-	@PutMapping("/{id}/comments/{userCommentId}")
-	public void updateUserComment(@PathVariable UUID id, @PathVariable UUID userCommentId, @RequestBody CommentRequest commentRequest) {
-		commentService.updateUserComment(id, userCommentId, commentRequest);
-	}
-	
 	@DeleteMapping("/{id}")
 	public void deletePost(@PathVariable UUID id) {
 		postService.deletePost(id);
-	}
-	
-	@DeleteMapping("/{id}/comments/{userCommentId}")
-	public void deletePost(@PathVariable UUID id, @PathVariable UUID userCommentId) {
-		commentService.deleteUserComment(id, userCommentId);
 	}
 }
