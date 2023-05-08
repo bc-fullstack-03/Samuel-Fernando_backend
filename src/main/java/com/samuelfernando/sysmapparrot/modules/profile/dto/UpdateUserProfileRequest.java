d97bcfd611f00ebda61f176fbf622817d836824d
SamuelFST <guti.samuel2002@gmail.com>
2023-05-08 14:11:45 -0300

    updateUserProfileRequest dto created

---
+++ src/main/java/com/samuelfernando/sysmapparrot/modules/profile/dto/UpdateUserProfileRequest.java
@@ -0,0 +1,12 @@
+package com.samuelfernando.sysmapparrot.modules.profile.dto;
+
+public class UpdateUserProfileRequest {
+	public String name;
+	
+	public UpdateUserProfileRequest() {
+	}
+
+	public UpdateUserProfileRequest(String name) {
+		this.name = name;
+	}
+}
